d4dfa51cb0435c2f584752486d4a02679f77f7ce
Anmol786 <54316673+Anmol786@users.noreply.github.com>
2020-10-14 01:23:39 +0530

    Integration complete from backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat'
+import Pusher from 'pusher-js';
+import axios from './axios';
 
 
 function App() {
+
+  const [messages,setMessages] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/messages/sync')
+    .then(resposnse =>{
+      setMessages(resposnse.data);
+    })
+  }, []);
+
+  useEffect(()=>{
+    const pusher = new Pusher('f75c5d8880e10edcb095', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(newMessages) {
+      setMessages([...messages,newMessages])
+    });
+
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  },[messages])
+console.log(messages);
   return (
     <div className="app">
       <div className="app_body">
         <Sidebar/>
-        <Chat/>
+        <Chat messages={messages}/>
       </div>
     </div>
   );
--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './chat.css';
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
@@ -6,8 +6,23 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import MicIcon from '@material-ui/icons/Mic';
 import { Avatar, IconButton } from '@material-ui/core';
+import axios from './axios';
 
-const Chat = ()=>{
+const Chat = ({messages})=>{
+
+    const [input,setInput] = useState("");
+
+    const send_message = async(e)=>{
+        e.preventDefault();
+        await axios.post("messages/new",{
+            name:"Anmol",
+            message:input,
+            timestamp:new Date().toUTCString(),
+            received:false
+        })
+        setInput('');
+    };
+
     return(
         <>
         <div className="chat">
@@ -30,34 +45,23 @@ const Chat = ()=>{
                 </div>
             </div>
             <div className="chat_body">
-                <p className="chat_message ">
-                    <span className="name">Anmol</span>
-                    A message
+                {messages.map((message)=>(
+                    <p className={`chat_message ${message.received && "chat_recieved"}`}>
+                    <span className="name">{message.name}</span>
+                    {message.message}
                     <span className="chat_time">{new Date().toUTCString()}</span>
                 </p>
-                <p className="chat_message chat_recieved ">
-                    <span className="name">Anmol</span>
-                    A message
-                    <span className="chat_time">{new Date().toUTCString()}</span>
-                </p>
-                <p className="chat_message ">
-                    <span className="name">Anmol</span>
-                    A message
-                    <span className="chat_time">{new Date().toUTCString()}</span>
-                </p>
-                <p className="chat_message ">
-                    <span className="name">Anmol</span>
-                    A message
-                    <span className="chat_time">{new Date().toUTCString()}</span>
-                </p>
+                ))}
             </div>
             <div className="chat_footer">
                 <IconButton>
                 <InsertEmoticonIcon/>
                 </IconButton>
                 <form>
-                    <input type="text" placeholder="Type a message"/>
-                    <button type="submit" >send</button>
+                    <input value = {input} 
+                    onChange ={(e)=> setInput(e.target.value)}
+                    type="text" placeholder="Type a message"/>
+                    <button onClick={send_message} type="submit" >send</button>
                 </form>
                 <IconButton>
                 <MicIcon/>
